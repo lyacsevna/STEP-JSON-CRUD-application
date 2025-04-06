dc98c3a17d377f5e45b9137c55d7e27b04eba166
Sofia <sonechka68zvr@gmail.com>
2025-04-06 15:26:55 +0300

    chore: code cleanup using Visual Studio tools
    - Removed unused namespaces
    - Fixed indentation and formatting
    - Removed unnecessary comments

--- SchemaManager.cs
+++ SchemaManager.cs
@@ -2,9 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-using System.Windows;
 using System.Windows.Shapes;
 
 namespace STEP_JSON_Application_for_ASKON
